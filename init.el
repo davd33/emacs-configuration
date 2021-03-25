f3e12c2e722892b65fa5ae59c9a62c4ea465cae5
David Rueda <davd33@gmail.com>
2021-03-25 13:56:04 +0100

    tide - typescript

--- init.el
+++ init.el
@@ -220,6 +220,27 @@
                                 (tern-ac-setup))))
                           ((tern-auto-complete :config-group :js))
                           ((tern-context-coloring :config-group :js))
+                          ((tide :config-group :js)
+                           :config
+                           (defun setup-tide-mode ()
+                             (interactive)
+                             (tide-setup)
+                             (flycheck-mode +1)
+                             (setq flycheck-check-syntax-automatically '(save mode-enabled))
+                             (eldoc-mode +1)
+                             (tide-hl-identifier-mode +1)
+                             ;; company is an optional dependency. You have to
+                             ;; install it separately via package-install
+                             ;; `M-x package-install [ret] company`
+                             (company-mode +1))
+
+                           ;; aligns annotation to the right hand side
+                           (setq company-tooltip-align-annotations t)
+
+                           ;; formats the buffer before saving
+                           (add-hook 'before-save-hook 'tide-format-before-save)
+
+                           (add-hook 'typescript-mode-hook #'setup-tide-mode))
                           ;; MATRIX CHAT
                           ;; (matrix-client
                           ;;  :quelpa (matrix-client :fetcher github :repo "alphapapa/matrix-client.el"
